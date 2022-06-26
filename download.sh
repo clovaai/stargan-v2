875b70a150609e8a678ed8482562e7074cdce7e5
Yunjey Choi <yunjey.choi@gmail.com>
2022-06-26 19:53:12 +0900

    Update download.sh

--- download.sh
+++ download.sh
@@ -48,7 +48,8 @@ elif  [ $FILE == "afhq-dataset" ]; then
     rm $ZIP_FILE
 
 elif  [ $FILE == "afhq-v2-dataset" ]; then
-    URL=https://www.dropbox.com/s/scckftx13grwmiv/afhq_v2.zip?dl=0
+    #URL=https://www.dropbox.com/s/scckftx13grwmiv/afhq_v2.zip?dl=0
+    URL=https://www.dropbox.com/s/vkzjokiwof5h8w6/afhq_v2.zip?dl=0
     ZIP_FILE=./data/afhq_v2.zip
     mkdir -p ./data
     wget -N $URL -O $ZIP_FILE
